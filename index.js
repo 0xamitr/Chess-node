0a4509f41dcef5eaf40a6bd33bbba44972edb405
0xamitr <0xamitr@gmail.com>
2024-09-23 21:40:17 +0530

    asdf

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = createServer(app);
 const io = new Server(server, {
     cors: {
         origin: [
-            'https://chess-self-two.vercel.app',
+            'http://chess-self-two.vercel.app',
             'http://localhost:3000',
         ],
         methods: ['GET', 'POST'],
