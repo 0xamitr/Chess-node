7043285e70e10b627bec1c95403cde20ff3c07c7
0xamitr <0xamitr@gmail.com>
2024-09-23 22:46:40 +0530

    fuck

--- index.js
+++ index.js
@@ -14,12 +14,6 @@ const io = new Server(server, {
     }
 });
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 app.use(express.json());
 
 setInterval(()=>{
