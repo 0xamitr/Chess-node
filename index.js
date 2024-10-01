899075227a2caaff092781c7f325aa89cae13eab
0xamitr <0xamitr@gmail.com>
2024-10-02 01:06:39 +0530

    https

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { Server } from 'socket.io';
-import { createServer } from 'http';
+import { createServer } from 'https';
 import fs from 'fs';
 
 const app = express();
